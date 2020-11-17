75f6094bcd3c6948d5353cf0e6afc38d228e9e7d
luiz filipe de moura <luizfilipeholiday@gmail.com>
2020-11-17 16:58:53 -0400

    Initial commit

---
+++ src/main/java/br/com/caelum/vraptor/controller/AngendEADController.java
@@ -0,0 +1,12 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Path;
+
+@br.com.caelum.vraptor.Controller
+public class AngendEADController {
+    @Path("/")
+    public void inicio(){
+
+    }
+
+}
