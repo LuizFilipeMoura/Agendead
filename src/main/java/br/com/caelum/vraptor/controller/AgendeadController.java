4aab37e9abeeae0466eb1682ae5637848f0b3950
luiz filipe de moura <luizfilipeholiday@gmail.com>
2020-11-17 17:12:20 -0400

    Pagina criada

--- src/main/java/br/com/caelum/vraptor/controller/AgendeadController.java
+++ src/main/java/br/com/caelum/vraptor/controller/AgendeadController.java
@@ -3,7 +3,7 @@ package br.com.caelum.vraptor.controller;
 import br.com.caelum.vraptor.Path;
 
 @br.com.caelum.vraptor.Controller
-public class AngendEADController {
+public class AgendeadController {
     @Path("/")
     public void inicio(){
 
