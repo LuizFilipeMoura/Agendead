204563ef8f755024e5588fb908da77e013469fc8
Murilo Mamede <muriloomamede@gmail.com>
2020-11-17 20:53:41 -0400

    Aluno model criado

---
+++ src/main/java/br/com/caelum/vraptor/dao/AlunoDao.java
@@ -0,0 +1,33 @@
+package br.com.caelum.vraptor.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.vraptor.model.Aluno;
+
+public class AlunoDao {
+
+    private final EntityManager em;
+
+    public AlunoDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void adiciona(Aluno aluno) {
+        em.persist(aluno);
+    }
+
+    public void remove(Aluno aluno) {
+        em.remove(busca(aluno));
+    }
+
+    public Aluno busca(Aluno aluno) {
+        return em.find(Aluno.class, aluno.getId());
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<Aluno> lista() {
+        return em.createQuery("select p from aluno p").getResultList();
+    }
+}
